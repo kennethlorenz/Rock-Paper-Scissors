4cca55e2159f565b84be765e5c4c222e61f4b4ec
Kenneth <kennethlorenzg@gmail.com>
2023-09-06 22:29:00 -0400

    Add play round function.
    Function that allows users to play a round given the selection of a player.

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
-// Function for computer choice
-function getComputerChoice() {
-  const computerChoices = ["Rock", "Paper", "Scissors"];
+// Function for getting a computer choice for either rock paper scissors
+function getComputerChoice(playerSelection) {
+  var computerChoices = ["rock", "paper", "scissors"];
+
+  selections = computerChoices.filter((choice) => choice !== playerSelection);
+  console.log(selections);
   //   randomly select an choice from computer selection
-
   var computerChoice =
-    computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
+    selections[Math.floor(Math.random() * selections.length)];
+  console.log(computerChoice);
   return computerChoice;
 }
 
-console.log(getComputerChoice());
+// function to play a round that accepts a player selection and a computer selection
+function playRound(playerSelection, computerSelection) {
+  const player = playerSelection.toLowerCase();
+
+  //   function to call if player selects rock
+  function playerSelectedRock() {
+    if (computerSelection == "paper") {
+      return "You Lost! Paper beats Rock!";
+    } else if (computerSelection == "scissors") {
+      return "You win! Rock beats Scissors!";
+    }
+  }
+
+  //   function to call if player selects paper
+  function playerSelectedPaper() {
+    if (computerSelection == "scissors") {
+      return "You Lost! Scissors beats Paper!";
+    } else if (computerSelection == "rock") {
+      return "You win! Paper beats Rock!";
+    }
+  }
+
+  //   function to call if player selects scissors
+  function playerSelectedScissors() {
+    if (computerSelection == "rock") {
+      return "You Lost! Rock beats Scissors!";
+    } else if (computerSelection == "paper") {
+      return "You win! Scissors beats Paper";
+    }
+  }
+
+  // condition to check if a player has selected either rock paper or scissors.
+  if (player == "rock") {
+    return playerSelectedRock();
+  } else if (player == "paper") {
+    return playerSelectedPaper();
+  } else if (player == "scissors") {
+    return playerSelectedScissors();
+  }
+}
+
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice(playerSelection);
+console.log(playRound(playerSelection, computerSelection));
